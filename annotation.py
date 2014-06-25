9398d4a18ef1341c99c42d5d01f8702fcac01cb0
Ritwik Gupta <ritwikg2004@live.com>
2014-06-25 09:55:22 -0400

    added proper annotation.py with fixes to take cmd line input

--- annotation.py
+++ annotation.py
@@ -1,14 +1,52 @@
+import sys
+
 tss = open("tissueSourceSite.txt", "r")
-matrix = open("genomicMatrixTrans", "r")
-out = open("tumorsAnnotated.txt", "w")
-next(matrix)
+common = open(sys.argv[1], "r")
 
+tlcSet = set()
+tcgaSet = set()
 tumDict = {}
+mergedList = []
+newList = []
 
-for line in tss:
-	tumDict{str(line.strip().split("\t")[1]): list()}
-	for line2 in matrix:
-		if line.strip().split("\t")[0] == line2.strip().split("\t")[0][5:7]:
-			tumDict{str(line.strip().split("\t")[1]): tumDict{str(line.strip().split("\t")[1])}.append(line2.strip().split("\t")[0])}
+for codes in tss:
+	twoLetCode = {codes.split("\t")[0]}
+	tlcSet.update(twoLetCode)
+	mergedList.append([codes.split("\t")[0], codes.split("\t")[2], set()])
 
-print tumDict
+for i in range(0, len(mergedList)):
+	for j in range(i + 1, len(mergedList)):
+		if mergedList[i][1] == mergedList[j][1]:
+			temp =  (mergedList[i][1], mergedList[i][2].union(mergedList[j][2]))
+			newList.append(temp)
+newNewList = []
+for i in newList:
+	if i not in newNewList:
+		newNewList.append(i)
+
+for row in common:
+	tcgaId = {row.strip()}
+	tcgaSet.update(tcgaId)
+
+while len(tcgaSet) != 0:
+	temp = tcgaSet.pop()
+	for i in range(0, len(mergedList)):
+		if mergedList[i][0] == temp.split("-")[1]:
+			for tup in newNewList:
+				if mergedList[i][1] == tup[0]:
+					tup[1].update({temp})
+
+print newNewList
+
+with open("tumorsAnnotated.txt", "w") as out:
+	for i in newNewList:
+		out.write(str(i[0] + "\t"))
+		tempStr = ""
+		while len(i[1]) != 0:
+			tempStr += str(i[1].pop()) + "\t"
+		out.write(tempStr + "\n")
+ 
+#Because Dr. Lu told me to
+tss.close()
+common.close()
+out.close()
