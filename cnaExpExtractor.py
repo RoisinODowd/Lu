3fd73dd9dd50827d6a68eb5123154b218b701fa8
Kevin <kevin@lu-dt-024.(none)>
2014-07-17 13:31:14 -0400

    crap commit

--- cnaExpExtractor.py
+++ cnaExpExtractor.py
@@ -1,25 +1,60 @@
 import sys
 
+"""
+Improper argument handling
+"""
+if len(sys.argv) != 6:
+	sys.stderr.write("The proper usage of this program is 'python cnaExpExtractor.py [CNA] [EXPRESSIONS] [CommonTumors] [OutputCNA] [OutputExpression]'\n")
+	sys.exit()
+
+"""
+Open five files, all tumor x gene, the first being the
+CNA matrix, the second the expression matrix, the third
+the commonTumors list, the fourth the output cnaCommons
+matrix, and the fifth the expCommons matrix.
+"""
 cnaMatrixTrans = open(sys.argv[1], "r")
 expMatrixTrans = open(sys.argv[2], "r")
 comTumorList = open(sys.argv[3], "r")
 cnaComMatOut = open(sys.argv[4], "w")
 expComMatOut = open(sys.argv[5], "w")
 
+"""
+Declarations
+"""
 cnaDict = {}
 expDict = {}
 
-header = cnaMatrixTrans.readline()
-next(expMatrixTrans)
-cnaComMatOut.write(header)
-expComMatOut.write(header)
+"""
+Read the header from the CNA matrix and the exp Matrix
+and output them to the common matrices.
 
+cnaHeader = 'Text\tText\tText...'
+expHeader = 'Text\tText\tText...'
+"""
+cnaHeader = cnaMatrixTrans.readline()
+expHeader = expMatrixTrans.readline()
+cnaComMatOut.write(cnaHeader)
+expComMatOut.write(expHeader)
+
+"""
+Adding values to cnaDict and expDict, where the key is
+the TCGA id and the value is the rest of the line.
+
+cnaDict = {"TCGA ID": "Line", "TCGA ID": "Line", ...}
+expDict = {"TCGA ID": "Line", "TCGA ID": "Line", ...}
+"""
 for line in cnaMatrixTrans:
 	cnaDict[str(line.split("\t")[0])] = str(line[15:])
 
 for line in expMatrixTrans:
 	expDict[str(line.split("\t")[0])] = str(line[15:])
 
+"""
+For each line in the commonTumors list, if the id is in
+both the cnaDict and expDict, then the whole line is
+outputted to cnaComMatOut and expComMatOut.
+"""
 for line in comTumorList:
 	if line.strip() in cnaDict and line.strip() in expDict:
 		cnaComMatOut.write(line.strip() + cnaDict[str(line.strip())])
@@ -29,6 +64,9 @@ comTumorList.seek(0)
 for line in comTumorList:
 	if line.strip() in expDict and line.strip() in cnaDict:
 		expComMatOut.write(line.strip() + expDict[str(line.strip())])
-		
+
+"""
+Closing files
+"""
 cnaComMatOut.close()
 expComMatOut.close()
