7c2b846f2d5d7061bfb49bf34491c3e5e55dcc6d
Ritwik Gupta <ritwikg2004@live.com>
2014-07-17 13:52:01 -0400

    made reducer.py work with transposed matrices

--- reducer.py
+++ reducer.py
@@ -7,29 +7,32 @@ if len(sys.argv) != 5:
 	sys.stderr.write("The proper usage of this program is 'python reducer.py [MUTATIONS] [EXPRESSIONS] [CNA] [OutputFile]'\n")
 	sys.exit()
 
-print "Please make sure that all matrices have genes as rows and tumors as columns, aka untransposed. If you aren't sure, check now."
-
 """
 Opens multiple files via the command line.
 The mutation matrix is the first argument, the expression the second,
-and the CNA the third.
+and the CNA the third. The first line in each file is skipped.
 """
 mutFile = open(sys.argv[1], "r")
 expFile = open(sys.argv[2], "r")
 cnaFile = open(sys.argv[3], "r")
+next(mutFile)
+next(expFile)
+next(cnaFile)
 
 """
-The first line of each line is read, stripped, and split by tabs
-so each tumor is its own element in a list.
+The first column of each line, the TCGA id, is appended to the
+respective list.
 
 mutList = ['Tumor1', 'Tumor2', ...]
 expList = ['Tumor1', 'Tumor2', ...]
 cnaList = ['Tumor1', 'Tumor2', ...]
 """
-mutList = mutFile.readline().strip().split("\t")
-expList = expFile.readline().strip().split("\t")
-cnaList = cnaFile.readline().strip().split("\t")
-
+for line in mutFile:
+	mutList.append(line.split("\t")[0])
+for line in expFile:
+	expList.append(line.split("\t")[0])
+for line in cnaFile:
+	cnaList.append(line.split("\t")[0])
 """
 Each list is turned into a set to remove duplicates
 and to allow for the usage of set mathematical functions.
